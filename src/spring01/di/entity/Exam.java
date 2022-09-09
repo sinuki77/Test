11fe0cf1a20d718cc0b50eecb3f0b3cf997484be
sinuki77 <113156757+sinuki77@users.noreply.github.com>
2022-09-09 13:38:17 +0900

    spring_practice
    first Test

---
+++ src/spring01/di/entity/Exam.java
@@ -0,0 +1,7 @@
+package spring01.di.entity;
+
+public interface Exam {
+
+	int total();
+	float avg();
+}
---
+++ src/spring01/di/ui/ExamConsole.java
@@ -0,0 +1,10 @@
+package spring01.di.ui;
+
+import spring01.di.entity.Exam;
+
+public interface ExamConsole {
+
+	void print();
+
+	void setExam(Exam exam);
+}
